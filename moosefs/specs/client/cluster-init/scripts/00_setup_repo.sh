d3b148bfdd0bb5c3848611c6f2433b75c5684fdb
Tomasz Jozefiak <tojozefi@microsoft.com>
2020-09-26 01:54:35 +0200

    Update 00_setup_repo.sh

--- moosefs/specs/client/cluster-init/scripts/00_setup_repo.sh
+++ moosefs/specs/client/cluster-init/scripts/00_setup_repo.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-wget -O - https://ppa.moosefs.com/moosefs.key | apt-key add -
-echo "deb http://ppa.moosefs.com/moosefs-3/apt/ubuntu/bionic bionic main" > /etc/apt/sources.list.d/moosefs.list
-apt update
+source /etc/os-release
+if [[ "$ID" == "ubuntu" ]]; then
+	wget -O - https://ppa.moosefs.com/moosefs.key | apt-key add -
+	echo "deb http://ppa.moosefs.com/moosefs-3/apt/ubuntu/bionic bionic main" > /etc/apt/sources.list.d/moosefs.list
+	apt update
+elif [[ "$ID" == "centos" ]]; then
+	curl "https://ppa.moosefs.com/RPM-GPG-KEY-MooseFS" > /etc/pki/rpm-gpg/RPM-GPG-KEY-MooseFS
+	curl "http://ppa.moosefs.com/MooseFS-3-el${VERSION_ID}.repo" > /etc/yum.repos.d/MooseFS.repo
+else
+	echo "unsupported OS" >&2
+	exit 1
+fi
