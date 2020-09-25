11c4895f25f69d6b78cd972549722643882e915b
Tomasz Jozefiak <tojozefi@microsoft.com>
2020-09-26 01:53:36 +0200

    Update 01_install_client.sh

--- moosefs/specs/client/cluster-init/scripts/01_install_client.sh
+++ moosefs/specs/client/cluster-init/scripts/01_install_client.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-moosefsroot=$(jetpack config moosefs.rootdir)
+moosefsmount=$(jetpack config moosefs.mountpoint)
 masterip=$(jetpack config moosefs.masterip)
 
-apt install moosefs-client
-mkdir $moosefsroot
-chmod 777 $moosefsroot
-mfsmount $moosefsroot -H $masterip
+source /etc/os-release
+if [[ "$ID" == "ubuntu" ]]; then
+	apt install moosefs-client
+elif [[ "$ID" == "centos" ]]; then
+	yum install -y moosefs-client
+else
+	echo "unsupported OS" >&2
+	exit 1	
+fi
+
+mkdir -p $moosefsmount
+chmod 777 $moosefsmount
+mfsmount $moosefsmount -H $masterip
+
