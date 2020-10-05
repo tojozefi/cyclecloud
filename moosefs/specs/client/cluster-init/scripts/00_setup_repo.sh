fdd324bda83a009bdea71a39b58b442cfd987a56
Tomasz Jozefiak <tojozefi@microsoft.com>
2020-10-06 00:02:37 +0200

    fix ubuntu repos setup

--- moosefs/specs/client/cluster-init/scripts/00_setup_repo.sh
+++ moosefs/specs/client/cluster-init/scripts/00_setup_repo.sh
@@ -3,12 +3,22 @@
 source /etc/os-release
 if [[ "$ID" == "ubuntu" ]]; then
 	wget -O - https://ppa.moosefs.com/moosefs.key | apt-key add -
-	echo "deb http://ppa.moosefs.com/moosefs-3/apt/ubuntu/bionic bionic main" > /etc/apt/sources.list.d/moosefs.list
+	if [[ "$VERSION_ID" == "20.04" ]]; then
+		echo "deb http://ppa.moosefs.com/moosefs-3/apt/ubuntu/focal focal main" > /etc/apt/sources.list.d/moosefs.list
+	elif [[ "$VERSION_ID" == "18.04" ]]; then
+		echo "deb http://ppa.moosefs.com/moosefs-3/apt/ubuntu/bionic bionic main" > /etc/apt/sources.list.d/moosefs.list
+	elif [[ "$VERSION_ID" == "16.04" ]]; then
+		echo "deb http://ppa.moosefs.com/moosefs-3/apt/ubuntu/xenial xenial main" > /etc/apt/sources.list.d/moosefs.list
+	else
+		echo "unsupported OS" >&2
+		exit 1
+	fi
 	apt update
 elif [[ "$ID" == "centos" ]]; then
-	curl "https://ppa.moosefs.com/RPM-GPG-KEY-MooseFS" > /etc/pki/rpm-gpg/RPM-GPG-KEY-MooseFS
+  curl "https://ppa.moosefs.com/RPM-GPG-KEY-MooseFS" > /etc/pki/rpm-gpg/RPM-GPG-KEY-MooseFS
 	curl "http://ppa.moosefs.com/MooseFS-3-el${VERSION_ID}.repo" > /etc/yum.repos.d/MooseFS.repo
 else
 	echo "unsupported OS" >&2
 	exit 1
 fi
+
